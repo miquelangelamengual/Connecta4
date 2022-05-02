0ba704ddeb575a9aefba15d331649bca8de603f9
Miquel Angel <hulkrojo2@gmail.com>
2022-05-02 12:07:28 +0200

    made board creation modular

--- src/main/java/es/hulk/connecta4/board/Board.java
+++ src/main/java/es/hulk/connecta4/board/Board.java
@@ -4,14 +4,18 @@ import es.hulk.connecta4.box.Box;
 
 public class Board {
 
+    private int rows;
+    private int columns;
     private Box[][] board;
 
-    public Board() {
-        this.board = new Box[6][7];
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.board = new Box[rows][columns];
 
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 7; j++) {
-                board[i][j] = new Box(i, j);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.board[i][j] = new Box(i, j);
             }
         }
     }
