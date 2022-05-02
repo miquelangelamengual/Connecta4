146de9396f376280c6490dbd36edbb1f7663a059
Miquel Angel <hulkrojo2@gmail.com>
2022-05-02 11:52:56 +0200

    added box object

---
+++ src/main/java/es/hulk/connecta4/box/Box.java
@@ -0,0 +1,14 @@
+package es.hulk.connecta4.box;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class Box {
+
+    private boolean isEmpty;
+    private String color;
+
+}
