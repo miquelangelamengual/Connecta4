b9b4e7088dba69f0207561cf50fe0f6a2d48d3d3
Miquel Angel <hulkrojo2@gmail.com>
2022-05-04 09:06:25 +0200

    Added getters on main class

--- src/main/java/es/hulk/connecta4/Connecta4.java
+++ src/main/java/es/hulk/connecta4/Connecta4.java
@@ -2,15 +2,17 @@ package es.hulk.connecta4;
 
 import es.hulk.connecta4.board.Board;
 import es.hulk.connecta4.menus.MainMenu;
+import es.hulk.connecta4.utils.Utils;
+import lombok.Getter;
 
 public class Connecta4 {
 
-    private static Board board;
-    private static MainMenu menu;
+    @Getter private static Board board;
+    @Getter private static MainMenu menu;
+    @Getter private static Utils utils = new Utils();
 
     public static void main(String[] args) {
         menu = new MainMenu();
-        menu.selectPlayerName();
         board = menu.getBoard();
     }
 
