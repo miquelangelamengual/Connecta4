9d681dfc06df4cda6aa1dad99ea6973cb3fd3bc7
Miquel Angel <hulkrojo2@gmail.com>
2022-05-04 09:06:57 +0200

    added election menu (not sure what i will put there)

---
+++ src/main/java/es/hulk/connecta4/menus/ElectionMenu.java
@@ -0,0 +1,15 @@
+package es.hulk.connecta4.menus;
+
+import es.hulk.connecta4.Connecta4;
+import es.hulk.connecta4.board.Board;
+
+public class ElectionMenu {
+
+    private MainMenu menu = Connecta4.getMenu();
+    private Board board = menu.getBoard();
+
+    public void putIntoBoard() {
+        int column = 0;
+    }
+
+}
