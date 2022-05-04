d582c89fcbd56bd552530f2a3e99bbcdc563b35b
Miquel Angel <hulkrojo2@gmail.com>
2022-05-04 09:07:57 +0200

    utils class used to get text on other classes

--- src/main/java/es/hulk/connecta4/utils/Utils.java
+++ src/main/java/es/hulk/connecta4/utils/Utils.java
@@ -1,4 +1,11 @@
 package es.hulk.connecta4.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Utils {
+
+    private Text text = new Text();
 }
